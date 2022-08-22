9e4e72f1a3ab671f63cadf8c64070ee1f2667631
shane <shanestevensxk9@gmail.com>
2022-08-22 14:33:21 +0200

    update

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.use((req, res, next)=>{
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Headers", "*");
     res.setHeader("Access-Control-Allow-Methods", "*");
-    res.setHeader("Access-Control-Allow-*", "*");
+
     next();
 });
 
@@ -237,5 +237,3 @@ router.put("/users/:id", bodyParser.json(), async (req, res) => {
       }
     });
   });
-
-//   PRODUCTS

2594533084885756e98dad0e6fa4e096d33bdb0f
BEASTxk9 <shanestevensxk9@gmail.com>
2022-08-30 14:28:38 +0200

    update

--- index.js
+++ index.js
@@ -127,7 +127,7 @@ app.post('/login', bodyParser.json(),
         WHERE email = '${email}';
         `;
             db.query(strQry, async (err, results) => {
-                if (err) res.send(err);
+                if (err) throw err;
                 // const key = jwt.sign(JSON.stringify(results[0]), process.env.secret);
                 // res.json({
                 //     status: 200,
@@ -135,7 +135,6 @@ app.post('/login', bodyParser.json(),
                 // });
                 // localStorage.setItem('key', JSON.stringify(key));
                 // key = localStorage.getItem('key');
-          
                 switch (true) {
                     case (await compare(password, results[0].password)):
                         res.send('logged in successfull')
